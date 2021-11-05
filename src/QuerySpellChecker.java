90ec8c1b3fa2713a37d3c95f83847ed4d1ff5219
Kadjel <kadjel.ramkisoen@gmail.com>
2021-11-05 18:05:36 +0100

    Language detection

--- src/QuerySpellChecker.java
+++ src/QuerySpellChecker.java
@@ -1,12 +1,25 @@
-import org.apache.lucene.search.SpellChecker;
+import org.apache.lucene.index.IndexReader;
+import org.apache.lucene.search.spell.LuceneDictionary;
+import org.apache.lucene.search.spell.PlainTextDictionary;
+import org.apache.lucene.search.spell.SpellChecker;
+import org.apache.lucene.store.Directory;
+import org.apache.lucene.store.RAMDirectory;
 
 import java.io.File;
+import java.io.IOException;
 
 public class QuerySpellChecker {
-    SpellChecker spellchecker = new SpellChecker(spellIndexDirectory);
-    // To index a field of a user index:
-  spellchecker.indexDictionary(new LuceneDictionary(my_lucene_reader, a_field));
-    // To index a file containing words:
-  spellchecker.indexDictionary(new PlainTextDictionary(new File("myfile.txt")));
-    String[] suggestions = spellchecker.suggestSimilar("misspelt", 5);
+
+    public static void LuceneSpellChecking(IndexReader reader, String field) throws IOException {
+        Directory spellIndexDirectory = new RAMDirectory();
+        SpellChecker spellchecker = new SpellChecker(spellIndexDirectory);
+        // To index a field of a user index:
+        spellchecker.indexDictionary(new LuceneDictionary(reader, field));
+        // To index a file containing words:
+        spellchecker.indexDictionary(new PlainTextDictionary(new File("myfile.txt")));
+        String[] suggestions = spellchecker.suggestSimilar("misspelt", 5);
+        
+    }
+
+
 }
