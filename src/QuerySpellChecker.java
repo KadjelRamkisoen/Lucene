0f311112ab2e1dcfd98a3c3a561a91594f3ec87b
Katerina <ekimak2012@gmail.com>
2021-11-05 18:25:30 +0200

    spell checker

--- src/QuerySpellChecker.java
+++ src/QuerySpellChecker.java
@@ -1,12 +1,38 @@
-import org.apache.lucene.search.SpellChecker;
+
+import org.apache.lucene.analysis.core.WhitespaceAnalyzer;
+import org.apache.lucene.search.spell.PlainTextDictionary;
+import org.apache.lucene.search.spell.SpellChecker;
+import org.apache.lucene.store.Directory;
+import org.apache.lucene.store.FSDirectory;
+import org.apache.lucene.store.RAMDirectory;
+// https://github.com/dwyl/english-words/blob/master/words_alpha.txt - word list
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class QuerySpellChecker {
-    SpellChecker spellchecker = new SpellChecker(spellIndexDirectory);
-    // To index a field of a user index:
-  spellchecker.indexDictionary(new LuceneDictionary(my_lucene_reader, a_field));
-    // To index a file containing words:
-  spellchecker.indexDictionary(new PlainTextDictionary(new File("myfile.txt")));
-    String[] suggestions = spellchecker.suggestSimilar("misspelt", 5);
+    PlainTextDictionary wordDict;
+    SpellChecker spellchecker;
+
+    public QuerySpellChecker() throws IOException {
+        wordDict = new PlainTextDictionary(new File(".//words_alpha.txt"));
+        //File dir = new File(".//spell_checker.txt");
+        Path path = Paths.get(".//spell_checker.txt");
+        Directory directory = FSDirectory.open(path);
+        spellchecker = new SpellChecker(directory);
+
+        //spellchecker = new SpellChecker(new RAMDirectory());
+    }
+
+    public String[] querySpellCheckerCreation(String field) throws IOException {
+       //RAMDirectory dir = new RAMDirectory();
+
+        //Directory spellIndexDirectory = new SimpleFSDirectory(f.getPath());
+        //SpellChecker spellchecker = new SpellChecker(spellIndexDirectory);
+        spellchecker.indexDictionary(wordDict);
+        String[] suggestions = spellchecker.suggestSimilar(field, 5);
+        return suggestions;
+    }
 }
